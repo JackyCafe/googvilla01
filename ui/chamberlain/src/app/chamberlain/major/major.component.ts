e1de78b2ea376b74369393ffc69cbf01eb6ae571
yhlin <powerworker1234@gmail.com>
2023-08-28 14:46:09 +0800

    detail

--- ui/chamberlain/src/app/chamberlain/major/major.component.ts
+++ ui/chamberlain/src/app/chamberlain/major/major.component.ts
@@ -15,6 +15,11 @@ export class MajorComponent implements OnInit{
   subitems_id: any;
   id: any;
   works: any;
+  detail:number|undefined;
+  ActivateAddEditComp: boolean =false;
+  detailName: any;
+  user:number=1
+
 
   constructor(private service:SharedService) {
   }
@@ -41,18 +46,29 @@ export class MajorComponent implements OnInit{
   }
 
   openSubModal(item: any) {
+
     this.subitems_id = item.id;
     this.service.getDetailItem(this.subitems_id).subscribe(data=>{
       this.detailitems =data;
+
+
     });
   }
 
   openWorkRecord(item:any){
-    this.id = item.id
-    this.service.getWorkRecord(this.id).subscribe(data=>{
+    //detail 是一個字串，會出bug
+    this.detailName = item.detail
+    this.detail = item.id
+    this.ActivateAddEditComp=true;
+    this.service.getWorkRecord(this.detail).subscribe(data=>{
       this.works = data;
+      //console.log(this.works)
     })
 
   }
 
+  closeClick() {
+    this.ActivateAddEditComp=false;
+    // this.refreshDepList();
+  }
 }
--- ui/chamberlain/src/app/chamberlain/workrecord/workrecord.component.ts
+++ ui/chamberlain/src/app/chamberlain/workrecord/workrecord.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component,OnInit, Input} from '@angular/core';
+import {SharedService} from "../../shared.service";
 
 @Component({
   selector: 'app-workrecord',
   templateUrl: './workrecord.component.html',
   styleUrls: ['./workrecord.component.css']
 })
-export class WorkrecordComponent {
+export class WorkrecordComponent implements  OnInit{
+  @Input() user: number | undefined
+  @Input() detailName: number |undefined
+  @Input() det:any;
 
+  bonus:number|undefined;
+  mood:number|undefined;
+
+  constructor(private service :SharedService) {
+  }
+  ngOnInit(): void {
+
+  }
+
+  addWorkRecord() {
+
+    const now = new Date()
+      var val ={
+        user:this.user,
+        detail:this.det[0].detail,
+        working_date:now.toLocaleDateString(),
+        start_time: now.toLocaleString(),
+        end_time:now.toLocaleString(),
+        spend_time:0,
+        bonus:this.bonus,
+        mood:this.mood
+      };
+      console.log("val")
+      console.log(val)
+  }
 }
